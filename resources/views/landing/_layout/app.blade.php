df1a39e1824118889210acfb4d0e456d085e9bf3
Tensu Qiuwulu <tensu104qiuwulu98@email.com>
2024-11-14 15:14:45 +0800

    fix: asset not loaded in ngrok

--- resources/views/landing/_layout/app.blade.php
+++ resources/views/landing/_layout/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="keywords" content="Ogani, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>UMKM Pekutatan</title>
 
     <!-- Google Font -->
