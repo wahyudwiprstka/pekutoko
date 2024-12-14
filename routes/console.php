4172439e17b8e22e8d71c542e98bd85f46175441
wahyudwiprstka <wahyudwiprstka@gmail.com>
2024-12-14 20:09:06 +0800

    add scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Models\Order;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    Order::where('order_status', 0)
+    ->where('created_at', '<', today()->subDay())
+    ->update(['order_status' => 4]);
+})->hourly();  
